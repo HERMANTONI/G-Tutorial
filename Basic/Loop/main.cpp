96e456deb86a5a0e51944157cf8346462cb14793
HERMANTONI <82546034+HERMANTONI@users.noreply.github.com>
2023-07-08 16:10:15 +0700

    Create main.cpp

---
+++ Basic/Loop/main.cpp
@@ -0,0 +1,20 @@
+/*Author: Hermantoni
+**Learn Together
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int loop_for(){
+	for(int a=0; a<10; a++){
+		cout << a << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char** argv) {
+  cout << "hello" << endl;
+  loop_for();
+  return 0; 
+}
