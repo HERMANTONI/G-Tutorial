41c6f5d2eb458bf822ed12c1d56b9b228729d76a
HERMANTONI <82546034+HERMANTONI@users.noreply.github.com>
2023-07-08 16:18:59 +0700

    Update main.cpp

--- Basic/Loop/main.cpp
+++ Basic/Loop/main.cpp
@@ -13,8 +13,19 @@ int loop_for(){
 	return 0;
 }
 
+int loop_while(){
+	int a = 10;
+	while(a<20){
+		cout << a << endl;
+		a += 1;
+	}
+	return 0;
+}
+
 int main(int argc, char** argv) {
   cout << "hello" << endl;
   loop_for();
+  cout << "hello" << endl;
+  loop_while();
   return 0; 
 }
